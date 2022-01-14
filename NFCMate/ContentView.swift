ba8cf8dccf1e369cc25e0ef03bbefa7cee62d7ac
Navemics <navemics@gmail.com>
2022-01-14 12:35:10 +0530

    Initial Commit

---
+++ NFCMate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NFCMate
+//
+//  Created by NAVEEN MADHAN on 1/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NFCMate/NFCMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCMateApp.swift
+//  NFCMate
+//
+//  Created by NAVEEN MADHAN on 1/14/22.
+//
+
+import SwiftUI
+
+@main
+struct NFCMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
